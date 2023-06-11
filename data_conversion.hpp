c57f0386ae51976be63b88618e50d1c95c945b53
Ahmed Salama <salamanetwork@gmail.com>
2023-06-11 11:38:45 +0400

    Create data_conversion.hpp

---
+++ data_conversion.hpp
@@ -0,0 +1,30 @@
+#ifndef __DATA_CONVERSION_H
+#define __DATA_CONVERSION_H
+
+/*
+    Data Conversion:
+        - Overflow
+        - Underoverflow
+*/
+
+void data_conversion()
+{
+    
+}
+
+
+
+
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
