e8e6773e4c85234c933ad1ddece7eb57f310daa7
Ahmed Salama <salamanetwork@gmail.com>
2023-06-17 15:06:13 +0400

    Create arrays.hpp

---
+++ arrays.hpp
@@ -0,0 +1,30 @@
+#ifndef __ARRAYS_H
+#define __ARRAYS_H
+
+#include <iostream>
+#include <iomanip>
+#include <ios>
+
+#include <cstdlib>
+
+#include <bitset>
+
+using namespace std;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
+
+
+
