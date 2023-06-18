e0937f77c096cb8029173a54a76d0c2591ed6e4d
Ahmed Salama <salamanetwork@gmail.com>
2023-06-18 09:53:17 +0400

    Create pointers.hpp

---
+++ pointers.hpp
@@ -0,0 +1,49 @@
+#ifndef __POINTERS_H
+#define __POINTERS_H
+
+#include <iostream>
+#include <iomanip>
+#include <ios>
+
+#include <ctime>
+#include <cstdlib>
+
+#include <bitset>
+
+using namespace std;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
+
+
+
+
+
+
+
+
+
