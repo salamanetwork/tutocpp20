409d857ac7b732e9dee3a5ac6b0a70841008c515
Ahmed Salama <salamanetwork@gmail.com>
2023-06-24 23:27:17 +0400

    Create dyn_mem_alloc.hpp

---
+++ dyn_mem_alloc.hpp
@@ -0,0 +1,38 @@
+#ifndef __DYNAMIC_MEMORY_ALLOCATION_H
+#define __DYNAMIC_MEMORY_ALLOCATION_H
+
+#include <iostream>
+#include <iomanip>
+#include <ios>
+
+#include <ctime>
+#include <cstdlib>
+#include <cstdint>
+
+#include <bitset>
+
+using namespace std;
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
+
+
+
+
+
+
+
+
+
