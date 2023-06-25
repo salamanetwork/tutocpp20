fc3c10d3ec286dcf8e16160055f35361bdbf45cc
Ahmed Salama <salamanetwork@gmail.com>
2023-06-25 22:50:20 +0400

    Create references.hpp

---
+++ references.hpp
@@ -0,0 +1,34 @@
+#ifndef __REFERENCES_H
+#define __REFERENCES_H
+
+#include <iostream>
+#include <iomanip>
+#include <ios>
+#include <array>
+#include <new>
+
+
+#include <ctime>
+#include <cstdlib>
+#include <cstdint>
+#include <climits>
+
+#include <bitset>
+
+using namespace std;
+
+
+
+
+
+
+#endif
+
+
+
+
+
+
+
+
+
