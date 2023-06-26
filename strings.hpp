291bfb00bc32bf491ee43e59d81c7bbfa9443812
Ahmed Salama <salamanetwork@gmail.com>
2023-06-26 20:51:04 +0400

    Create strings.hpp

---
+++ strings.hpp
@@ -0,0 +1,51 @@
+#ifndef __STRINGS_H
+#define __STRINGS_H
+
+#include <iostream>
+#include <iomanip>
+#include <ios>
+#include <array>
+#include <new>
+
+
+#include <ctime>
+#include <cstdlib>
+#include <cstdint>
+#include <climits>
+
+#include <bitset>
+
+using namespace std;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
+
+
+
+
+
+
+
+
+
