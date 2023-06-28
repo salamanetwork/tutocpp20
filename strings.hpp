1c00a94a8bcb25fdedab0862bf589c04cd185697
Ahmed Salama <salamanetwork@gmail.com>
2023-06-28 05:19:46 +0400

    Update strings.hpp
    /*
        - String Literals (#include <string_literals>):
            - namespace provides user-defined literals for creating std::string objects directly from string literals. 
            - It allows you to create std::string objects using a concise syntax without explicitly calling the std::string constructor. 
            - To use std::string_literals, you need to include the <string> and <string_literals> headers.    
        
        - https://learn.microsoft.com/en-us/cpp/cpp/string-and-character-literals-cpp?view=msvc-170
    */
    
    void cpp_string_literals();

--- strings.hpp
+++ strings.hpp
@@ -659,6 +659,70 @@ void cpp_string_02()
     std::cout << std::endl;
 }
 
+/*
+    - String Literals (#include <string_literals>):
+        - namespace provides user-defined literals for creating std::string objects directly from string literals. 
+        - It allows you to create std::string objects using a concise syntax without explicitly calling the std::string constructor. 
+        - To use std::string_literals, you need to include the <string> and <string_literals> headers.    
+    
+    - https://learn.microsoft.com/en-us/cpp/cpp/string-and-character-literals-cpp?view=msvc-170
+*/
+
+void cpp_string_literals()
+{
+    using namespace std::string_literals;
+
+    std::string str1 = "Hello, World!";  // Regular string literal
+    std::string str2 = "Hello, "s + "World!";  // User-defined literal
+
+    std::cout << "str1: " << str1 << std::endl;
+    std::cout << "str2: " << str2 << std::endl;
+
+    // Character literals
+    auto c0 =   'A'; // char
+    auto c1 = u8'A'; // char
+    auto c2 =  L'A'; // wchar_t
+    auto c3 =  u'A'; // char16_t
+    auto c4 =  U'A'; // char32_t
+
+    // Multicharacter literals
+    // warning: multi-character character constant [-Wmultichar]
+    // auto m0 = 'abcd'; // int, value 0x61626364
+
+
+    // String literals
+    auto s0 =   "hello"; // const char*
+    auto s1 = u8"hello"; // const char* before C++20, encoded as UTF-8,
+                         // const char8_t* in C++20
+    auto s2 =  L"hello"; // const wchar_t*
+    auto s3 =  u"hello"; // const char16_t*, encoded as UTF-16
+    auto s4 =  U"hello"; // const char32_t*, encoded as UTF-32
+
+    // Raw string literals containing unescaped \ and "
+    auto R0 =   R"("Hello \ world")"; // const char*
+    auto R1 = u8R"("Hello \ world")"; // const char* before C++20, encoded as UTF-8,
+                                      // const char8_t* in C++20
+    auto R2 =  LR"("Hello \ world")"; // const wchar_t*
+    auto R3 =  uR"("Hello \ world")"; // const char16_t*, encoded as UTF-16
+    auto R4 =  UR"("Hello \ world")"; // const char32_t*, encoded as UTF-32
+
+    // Combining string literals with standard s-suffix
+    auto S0 =   "hello"s; // std::string
+    auto S1 = u8"hello"s; // std::string before C++20, std::u8string in C++20
+    auto S2 =  L"hello"s; // std::wstring
+    auto S3 =  u"hello"s; // std::u16string
+    auto S4 =  U"hello"s; // std::u32string
+
+    // Combining raw string literals with standard s-suffix
+    auto S5 =   R"("Hello \ world")"s; // std::string from a raw const char*
+    auto S6 = u8R"("Hello \ world")"s; // std::string from a raw const char* before C++20, encoded as UTF-8,
+                                       // std::u8string in C++20
+    auto S7 =  LR"("Hello \ world")"s; // std::wstring from a raw const wchar_t*
+    auto S8 =  uR"("Hello \ world")"s; // std::u16string from a raw const char16_t*, encoded as UTF-16
+    auto S9 =  UR"("Hello \ world")"s; // std::u32string from a raw const char32_t*, encoded as UTF-32
+
+    
+}
 
 
 
