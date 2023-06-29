291483af4b5d2e891ae243690aa80c55464dfd38
Ahmed Salama <salamanetwork@gmail.com>
2023-06-29 11:25:35 +0400

    Create functions.hpp

---
+++ functions.hpp
@@ -0,0 +1,57 @@
+#ifndef __FUNCTIONS_H
+#define __FUNCTIONS_H
+
+#include <iostream>
+#include <bitset>
+#include <iomanip>
+#include <ios>
+#include <array>
+#include <new>
+#include <algorithm>
+#include <string>
+#include <string_view>
+
+
+#include <ctime>
+#include <cstdlib>
+#include <cstdint>
+#include <climits>
+#include <cctype>
+#include <cstring>
+
+
+using namespace std;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
+
+
+
+
+
+
+
+
+
