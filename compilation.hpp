e42dd3458a9c2480c62b370ec0b8f15213bc7b5f
Ahmed Salama <salamanetwork@gmail.com>
2023-06-29 14:40:19 +0400

    Create compilation.hpp

---
+++ compilation.hpp
@@ -0,0 +1,60 @@
+#ifndef __COMPILATION_H
+#define __COMPILATION_H
+
+#include <iostream>
+#include <bitset>
+#include <iomanip>
+#include <ios>
+#include <array>
+#include <new>
+#include <algorithm>
+#include <string>
+#include <string_view>
+
+
+#include <ctime>
+#include <cstdlib>
+#include <cstdint>
+#include <climits>
+#include <cctype>
+#include <cstring>
+
+
+using namespace std;
+
+/*
+    - Functions In C/C++:
+        
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
+
+
+
+
+
+
+
+
+
