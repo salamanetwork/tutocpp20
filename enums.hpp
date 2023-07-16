48631e9e513b04babe29f231dde603aceaa81dfe
Ahmed Salama <salamanetwork@gmail.com>
2023-07-16 14:45:27 +0400

    Create enums.hpp

---
+++ enums.hpp
@@ -0,0 +1,47 @@
+#ifndef __ENUMS_H
+#define __ENUMS_H
+
+#include <iostream>
+#include <bitset>
+#include <iomanip>
+#include <ios>
+#include <array>
+#include <new>
+#include <algorithm>
+#include <string>
+#include <string_view>
+
+
+#include <ctime>
+#include <cstdlib>
+#include <cstdint>
+#include <climits>
+#include <cctype>
+#include <cstring>
+
+
+using namespace std;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
+
+
+
+
