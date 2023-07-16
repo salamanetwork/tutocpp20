a15850b4560f15f9a22ba25f793ecc6ddbc52e90
Ahmed Salama <salamanetwork@gmail.com>
2023-07-16 17:39:08 +0400

    Create aliases.hpp

---
+++ aliases.hpp
@@ -0,0 +1,37 @@
+#ifndef __ALIASES_H
+#define __ALIASES_H
+
+#include <iostream>
+#include <bitset>
+#include <iomanip>
+#include <ios>
+#include <array>
+#include <new>
+#include <algorithm>
+#include <string>
+#include <string_view>
+
+
+#include <ctime>
+#include <cstdlib>
+#include <cstdint>
+#include <climits>
+#include <cctype>
+#include <cstring>
+
+
+using namespace std;
+
+
+
+
+
+
+
+
+
+#endif
+
+
+
+
