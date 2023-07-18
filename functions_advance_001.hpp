8c03e94b7b7cbeb2f979670d2995a14154627cbc
Ahmed Salama <salamanetwork@gmail.com>
2023-07-18 22:27:38 +0400

    Update functions_advance_001.hpp
    Inputs & Outputs Parameters In C++:
    void compute_sum_and_product(int a, int b, int& sum, int& product);
    void func_compute_sum_and_product();

--- functions_advance_001.hpp
+++ functions_advance_001.hpp
@@ -50,7 +50,62 @@
 
 using namespace std;
 
+/*
+    - Inputs & Outputs Parameters In C++:
+        - In C++, functions can have input parameters, output parameters, or both. 
+        - These parameters allow you to pass values to a function and receive values from a function. 
+        - Let's look at input and output parameters in detail:
+            - Input Parameters:
+                - Input parameters, also known as arguments, are used to pass values from the calling function to the called function. 
+                - They provide the necessary data for the function to perform its task. 
+                - In C++, input parameters are typically passed by value or by reference.
+                    - Pass by Value:
+                        - When a function parameter is passed by value, a copy of the argument's value is made and stored in a separate memory location. 
+                        - Any changes made to the parameter within the function do not affect the original argument.
+                    - Pass by Reference:
+                        - When a function parameter is passed by reference, the function receives a reference to the original argument. 
+                        - This allows the function to modify the argument, and any changes made to the parameter within the function affect the original argument.
+            - Output Parameters:
+                - Output parameters are used when a function needs to return multiple values or modify existing variables defined outside the function.
+                -  In C++, output parameters are typically passed by reference.
+        - Notes:
+            - It's worth noting that C++ supports additional techniques for returning values, such as returning a structure or using pointers. 
+            - However, input and output parameters, particularly pass by value and pass by reference, are commonly used and straightforward to understand.
+        - Syntax:
+            void functionName(parameterType parameter1, parameterType parameter2, ..., parameterType& outputParameter1, parameterType& outputParameter2, ...);
 
+            - In the prototype syntax:
+                - functionName is the name of the function.
+                - parameterType is the data type of the parameters.
+                - parameter1, parameter2, etc., are the input parameters.
+                - outputParameter1, outputParameter2, etc., are the output parameters passed by reference.
+        - Example:
+            void computeSumAndProduct(int a, int b, int& sum, int& product);
+*/
+
+// Function that calculates the sum and product of two integers
+void compute_sum_and_product(int a, int b, int& sum, int& product)
+{
+    sum = a + b;         // Calculate the sum of 'a' and 'b' and store the result in 'sum'
+    product = a * b;     // Calculate the product of 'a' and 'b' and store the result in 'product'
+}
+
+void func_compute_sum_and_product()
+{
+    int x = 5;            // Declare and initialize variable 'x' with the value 5
+    int y = 3;            // Declare and initialize variable 'y' with the value 3
+    int result_sum;       // Declare a variable 'result_sum' to store the sum
+    int result_product;   // Declare a variable 'result_product' to store the product
+
+    compute_sum_and_product(x, y, result_sum, result_product);
+    // Call the function 'compute_sum_and_product' passing 'x' and 'y' as input parameters
+    // and 'result_sum' and 'result_product' as output parameters
+
+    // Print the calculated sum and product
+    std::cout << "Sum: " << result_sum << std::endl;
+    std::cout << "Product: " << result_product << std::endl;
+
+}
 
 
 
