570379855d81b591e18700b0e2e2d51fe6c4049a
Ahmed Salama <salamanetwork@gmail.com>
2023-08-12 00:42:59 +0400

    Update templates.hpp

--- templates.hpp
+++ templates.hpp
@@ -292,4 +292,422 @@ using namespace std;
 */
 
 
+// Function template example with snake_case naming convention
+template <typename T>
+void cpp_func_template_print(T value) {
+    std::cout << value << std::endl;
+}
+
+// Template specialization example
+template <>
+void cpp_func_template_print<std::string>(std::string value) {
+    std::cout << "String version: " << value << std::endl;
+}
+
+// Non-type template parameter example
+template <typename T, int N>
+T cpp_func_template_power(T base) {
+    T result = 1;
+    for (int i = 0; i < N; ++i) {
+        result *= base;
+    }
+    return result;
+}
+
+// Template with constraints (C++20)
+template <typename T>
+requires std::is_integral_v<T>
+void cpp_func_template_process(T value) {
+    std::cout << "Processing integral value: " << value << std::endl;
+}
+
+// Variadic template example
+template <typename... Args>
+void cpp_func_template_log(Args... args) {
+    ((std::cout << args << " "), ...);
+    std::cout << std::endl;
+}
+
+void cpp_func_template_print_proc_log() {
+    cpp_func_template_print(5);
+    cpp_func_template_print(3.14);
+    cpp_func_template_print("Hello");
+
+    std::cout << cpp_func_template_power<int, 3>(2) << std::endl;
+
+    cpp_func_template_process(42);
+    // cpp_func_template_process(3.14); // This would cause a compilation error due to the constraint
+
+    cpp_func_template_log(1, 2, 3, "four", 5.5);
+
+}
+//  Let's consider a real-world example of a function template that performs element-wise addition for different types of arrays 
+// Function template for element-wise array addition
+template <typename T>
+std::vector<T> cpp_func_template_array_add(const std::vector<T>& arr1, const std::vector<T>& arr2) {
+    std::vector<T> result;
+    if (arr1.size() != arr2.size()) {
+        std::cerr << "Error: Arrays must have the same size for addition." << std::endl;
+        return result;
+    }
+
+    result.reserve(arr1.size());
+    for (size_t i = 0; i < arr1.size(); ++i) {
+        result.push_back(arr1[i] + arr2[i]);
+    }
+
+    return result;
+}
+
+void cpp_func_template_array_add_call() {
+    std::vector<int> intArray1 = {1, 2, 3, 4, 5};
+    std::vector<int> intArray2 = {5, 4, 3, 2, 1};
+
+    std::vector<double> doubleArray1 = {1.1, 2.2, 3.3, 4.4, 5.5};
+    std::vector<double> doubleArray2 = {0.5, 1.0, 1.5, 2.0, 2.5};
+
+    // Element-wise addition for int arrays
+    std::vector<int> intResult = cpp_func_template_array_add(intArray1, intArray2);
+    for (const auto& val : intResult) {
+        std::cout << val << " ";
+    }
+    std::cout << std::endl;
+
+    // Element-wise addition for double arrays
+    std::vector<double> doubleResult = cpp_func_template_array_add(doubleArray1, doubleArray2);
+    for (const auto& val : doubleResult) {
+        std::cout << val << " ";
+    }
+    std::cout << std::endl;
+}
+
+// Let's consider another real-world example of a function template. In this case, we'll create a function template that finds the maximum element in a container.
+
+// Function template to find the maximum element in a container
+template <typename Container>
+typename Container::value_type cpp_func_template_find_max(const Container& container) {
+    if (container.empty()) {
+        throw std::logic_error("Container is empty.");
+    }
+
+    auto maxElement = *std::max_element(container.begin(), container.end());
+    return maxElement;
+}
+
+void cpp_func_template_find_max_call() {
+    std::vector<int> intVector = {5, 12, 8, 20, 6};
+
+    try {
+        int maxInt = cpp_func_template_find_max(intVector);
+        std::cout << "Max element in intVector: " << maxInt << std::endl;
+    } catch (const std::logic_error& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
+    std::vector<double> doubleVector = {3.14, 1.618, 2.718, 0.577};
+
+    try {
+        double maxDouble = cpp_func_template_find_max(doubleVector);
+        std::cout << "Max element in doubleVector: " << maxDouble << std::endl;
+    } catch (const std::logic_error& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+}
+
+// Let's explore another real-world example of a function template. This time, we'll create a template for a mathematical function that calculates the average of a collection of values.
+// Function template to calculate the average of a collection of values
+template <typename T>
+T cpp_func_template_calculate_average(const std::vector<T>& values) {
+    if (values.empty()) {
+        throw std::logic_error("Cannot calculate average of an empty collection.");
+    }
+
+    T sum = 0;
+    for (const T& value : values) {
+        sum += value;
+    }
+
+    return sum / static_cast<T>(values.size());
+}
+
+void cpp_func_template_calculate_average_call() {
+    std::vector<int> intValues = {10, 20, 30, 40, 50};
+    std::vector<double> doubleValues = {3.5, 4.0, 5.5, 6.0, 7.5};
+
+    try {
+        int averageInt = cpp_func_template_calculate_average(intValues);
+        std::cout << "Average of intValues: " << averageInt << std::endl;
+    } catch (const std::logic_error& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
+    try {
+        double averageDouble = cpp_func_template_calculate_average(doubleValues);
+        std::cout << "Average of doubleValues: " << averageDouble << std::endl;
+    } catch (const std::logic_error& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+}
+
+// Let's explore another real-world example of a function template. In this scenario, we'll create a template for a sorting function that sorts elements of different types in ascending order.
+// Function template for sorting elements in ascending order
+template <typename T>
+void cpp_func_template_sort(std::vector<T>& elements) {
+    std::sort(elements.begin(), elements.end());
+}
+
+void cpp_func_template_sort_call() {
+    std::vector<int> intVector = {5, 2, 8, 1, 9};
+    std::vector<double> doubleVector = {3.14, 1.618, 2.718, 0.577};
+    std::vector<std::string> stringVector = {"apple", "banana", "grape", "orange"};
+
+    cpp_func_template_sort(intVector);
+    cpp_func_template_sort(doubleVector);
+    cpp_func_template_sort(stringVector);
+
+    std::cout << "Sorted intVector: ";
+    for (const auto& value : intVector) {
+        std::cout << value << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Sorted doubleVector: ";
+    for (const auto& value : doubleVector) {
+        std::cout << value << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Sorted stringVector: ";
+    for (const auto& value : stringVector) {
+        std::cout << value << " ";
+    }
+    std::cout << std::endl;
+}
+
+
+// Let's explore another real-world example of a function template. In this case, we'll create a template for a function that finds the first occurrence of a given value in a collection.
+// Function template to find the first occurrence of a value in a collection
+template <typename Container, typename T>
+typename Container::const_iterator cpp_func_template_find(const Container& container, const T& value) {
+    auto it = std::find(container.begin(), container.end(), value);
+    if (it != container.end()) {
+        return it;
+    }
+    return container.end();
+}
+
+// Function to calculate the index using iterators
+template <typename Container, typename Iterator>
+size_t calculate_index(const Container& container, Iterator it) {
+    return std::distance(container.begin(), it);
+}
+
+void cpp_func_template_find_call() {
+    std::vector<int> intVector = {5, 2, 8, 1, 9};
+    std::vector<double> doubleVector = {3.14, 1.618, 2.718, 0.577};
+    std::vector<std::string> stringVector = {"apple", "banana", "grape", "orange"};
+
+    int intToFind = 8;
+    auto intResult = cpp_func_template_find(intVector, intToFind);
+    size_t intIndex = intResult != intVector.end() ? calculate_index(intVector, intResult) : static_cast<size_t>(-1);
+    std::cout << "Found " << intToFind << " in intVector at index: " << intIndex << std::endl;
+
+    double doubleToFind = 2.718;
+    auto doubleResult = cpp_func_template_find(doubleVector, doubleToFind);
+    size_t doubleIndex = doubleResult != doubleVector.end() ? calculate_index(doubleVector, doubleResult) : static_cast<size_t>(-1);
+    std::cout << "Found " << doubleToFind << " in doubleVector at index: " << doubleIndex << std::endl;
+
+    std::string stringToFind = "banana";
+    auto stringResult = cpp_func_template_find(stringVector, stringToFind);
+    size_t stringIndex = stringResult != stringVector.end() ? calculate_index(stringVector, stringResult) : static_cast<size_t>(-1);
+    std::cout << "Found " << stringToFind << " in stringVector at index: " << stringIndex << std::endl;
+}
+
+// Let's explore another example, this time focusing on a function template that finds the minimum and maximum elements in a collection.
+// Function template to find the minimum and maximum elements in a collection
+template <typename Container, typename T = typename Container::value_type>
+std::pair<T, T> cpp_func_template_find_min_max(const Container& container) {
+    if (container.empty()) {
+        throw std::logic_error("Container is empty.");
+    }
+
+    T minElement = *std::min_element(container.begin(), container.end());
+    T maxElement = *std::max_element(container.begin(), container.end());
+
+    return std::make_pair(minElement, maxElement);
+}
+
+void cpp_func_template_find_min_max_call() {
+    std::vector<int> intVector = {5, 2, 8, 1, 9};
+    std::vector<double> doubleVector = {3.14, 1.618, 2.718, 0.577};
+    std::vector<std::string> stringVector = {"apple", "banana", "grape", "orange"};
+
+    try {
+        auto intMinMax = cpp_func_template_find_min_max(intVector);
+        std::cout << "Minimum in intVector: " << intMinMax.first << ", Maximum in intVector: " << intMinMax.second << std::endl;
+    } catch (const std::logic_error& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
+    try {
+        auto doubleMinMax = cpp_func_template_find_min_max(doubleVector);
+        std::cout << "Minimum in doubleVector: " << doubleMinMax.first << ", Maximum in doubleVector: " << doubleMinMax.second << std::endl;
+    } catch (const std::logic_error& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
+    try {
+        auto stringMinMax = cpp_func_template_find_min_max(stringVector);
+        std::cout << "Minimum in stringVector: " << stringMinMax.first << ", Maximum in stringVector: " << stringMinMax.second << std::endl;
+    } catch (const std::logic_error& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+}
+
+
+// Let's explore another example of a function template. This time, we'll create a template for a function that performs element-wise multiplication of two vectors and returns a new vector containing the result.
+// Function template for element-wise vector multiplication
+template <typename T>
+std::vector<T> cpp_func_template_vector_multiply(const std::vector<T>& vec1, const std::vector<T>& vec2) {
+    if (vec1.size() != vec2.size()) {
+        throw std::invalid_argument("Vectors must have the same size for multiplication.");
+    }
+
+    std::vector<T> result;
+    result.reserve(vec1.size());
+
+    for (size_t i = 0; i < vec1.size(); ++i) {
+        result.push_back(vec1[i] * vec2[i]);
+    }
+
+    return result;
+}
+
+void cpp_func_template_vector_multiply_call() {
+    std::vector<int> intVector1 = {1, 2, 3, 4, 5};
+    std::vector<int> intVector2 = {5, 4, 3, 2, 1};
+
+    std::vector<double> doubleVector1 = {1.1, 2.2, 3.3, 4.4, 5.5};
+    std::vector<double> doubleVector2 = {0.5, 1.0, 1.5, 2.0, 2.5};
+
+    try {
+        std::vector<int> intResult = cpp_func_template_vector_multiply(intVector1, intVector2);
+        std::cout << "Element-wise multiplication of int vectors: ";
+        for (const auto& val : intResult) {
+            std::cout << val << " ";
+        }
+        std::cout << std::endl;
+    } catch (const std::invalid_argument& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
+    try {
+        std::vector<double> doubleResult = cpp_func_template_vector_multiply(doubleVector1, doubleVector2);
+        std::cout << "Element-wise multiplication of double vectors: ";
+        for (const auto& val : doubleResult) {
+            std::cout << val << " ";
+        }
+        std::cout << std::endl;
+    } catch (const std::invalid_argument& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+}
+
+// In this example, we'll create a function template for a generic calculator that supports basic arithmetic operations.
+// Function template for a generic calculator
+template <typename T>
+T cpp_func_template_calculator(T a, T b, char operation) {
+    T result;
+    switch (operation) {
+        case '+':
+            result = a + b;
+            break;
+        case '-':
+            result = a - b;
+            break;
+        case '*':
+            result = a * b;
+            break;
+        case '/':
+            if (b != 0) {
+                result = a / b;
+            } else {
+                throw std::runtime_error("Division by zero.");
+            }
+            break;
+        default:
+            throw std::invalid_argument("Invalid operation.");
+    }
+    return result;
+}
+
+void cpp_func_template_calculator_call() {
+    try {
+        std::cout << "Result of 10 + 5: " << cpp_func_template_calculator(10, 5, '+') << std::endl;
+        std::cout << "Result of 15.5 - 7.2: " << cpp_func_template_calculator(15.5, 7.2, '-') << std::endl;
+        std::cout << "Result of 4 * 3: " << cpp_func_template_calculator(4, 3, '*') << std::endl;
+        std::cout << "Result of 25 / 5: " << cpp_func_template_calculator(25, 5, '/') << std::endl;
+        
+        // Uncommenting the line below will result in a division by zero error
+        // std::cout << "Result of 8 / 0: " << cpp_func_template_calculator(8, 0, '/') << std::endl;
+        
+        // Uncommenting the line below will result in an invalid operation error
+        // std::cout << "Result of 10 % 3: " << cpp_func_template_calculator(10, 3, '%') << std::endl;
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
+}
+
+// In this example, let's create a function template for a generic converter that converts temperature values between Fahrenheit and Celsius.
+// Function template for temperature conversion
+template <typename T>
+T cpp_func_template_convert_temperature(T temperature, char fromUnit, char toUnit) {
+    if (fromUnit == 'F' && toUnit == 'C') {
+        return static_cast<T>((temperature - 32) * 5 / 9);
+    } else if (fromUnit == 'C' && toUnit == 'F') {
+        return static_cast<T>((temperature * 9 / 5) + 32);
+    } else {
+        throw std::invalid_argument("Invalid temperature conversion units.");
+    }
+}
+
+void cpp_func_template_convert_temperature_call() {
+    try {
+        double fahrenheitTemp = 98.6;
+        double celsiusTemp = cpp_func_template_convert_temperature(fahrenheitTemp, 'F', 'C');
+        std::cout << fahrenheitTemp << " degrees Fahrenheit is " << celsiusTemp << " degrees Celsius." << std::endl;
+
+        double newFahrenheitTemp = cpp_func_template_convert_temperature(celsiusTemp, 'C', 'F');
+        std::cout << celsiusTemp << " degrees Celsius is " << newFahrenheitTemp << " degrees Fahrenheit." << std::endl;
+        
+        // Uncommenting the line below will result in an invalid temperature conversion units error
+        // double invalidTemp = cpp_func_template_convert_temperature(fahrenheitTemp, 'K', 'C');
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+}
+
+//  In this example, let's create a function template for a generic swap function that swaps the values of two variables.
+// Function template for swapping two values
+template <typename T>
+void cpp_func_template_swap(T& a, T& b) {
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+void cpp_func_template_swap_call() {
+    int intVar1 = 5, intVar2 = 10;
+    std::cout << "Before swap: intVar1 = " << intVar1 << ", intVar2 = " << intVar2 << std::endl;
+    cpp_func_template_swap(intVar1, intVar2);
+    std::cout << "After swap: intVar1 = " << intVar1 << ", intVar2 = " << intVar2 << std::endl;
+
+    double doubleVar1 = 3.14, doubleVar2 = 2.71;
+    std::cout << "Before swap: doubleVar1 = " << doubleVar1 << ", doubleVar2 = " << doubleVar2 << std::endl;
+    cpp_func_template_swap(doubleVar1, doubleVar2);
+    std::cout << "After swap: doubleVar1 = " << doubleVar1 << ", doubleVar2 = " << doubleVar2 << std::endl;
+}
+
+
 #endif
