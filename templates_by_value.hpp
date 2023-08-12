b8ac3de2e32bcfbc01f44e477ed4ceb21b912827
Ahmed Salama <salamanetwork@gmail.com>
2023-08-12 12:05:17 +0400

    Create templates_by_value.hpp

---
+++ templates_by_value.hpp
@@ -0,0 +1,63 @@
+#ifndef __TEMPLATES_BY_VALUE_H
+#define __TEMPLATES_BY_VALUE_H
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <deque>
+#include <list>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+#include <numeric>
+#include <functional>
+#include <iterator>
+#include <stdexcept>
+#include <exception>
+#include <limits>
+#include <random>
+#include <typeinfo>
+#include <tuple>
+#include <regex>
+#include <memory>
+#include <atomic>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <future>
+#include <chrono>
+#include <filesystem>
+#include <chrono>
+#include <optional>
+
+#include <cassert>
+#include <cstddef>
+#include <cwctype>
+#include <ctime>
+#include <cstdlib>
+#include <cstdint>
+#include <climits>
+#include <cctype>
+#include <cstring>
+#include <cmath>
+
+
+using namespace std;
+
+
+
+
+
+
+
+
+#endif
+
+
